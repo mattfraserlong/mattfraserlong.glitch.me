9c56667a8895954dbdc64f4d54413939b6726734
Glitch (mattfraserlong) <none>
2018-09-17 10:11:07 +0000

    🌘🚪 Updated with Glitch

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# mattfraserlong.glitch.me
-My sandbox site built on Glitch
+My invaders game with high score database
+
+
+Bugs:
+
+:-) change init() so it restarts the game totally
+
+
+App Todos:  
+:-) Highscores to appear at beginning of game, and end  
+:-) All players enter initials  
+:-) Initials limited to three characters  
+:-) Score sent to DB if larger than 5th highest score  
+:-) If score is in top 5, then congrats function triggered alongside gameover  
+:-) hide all js from browser with closure  
+:-) Change game to be more like space-invaders?  
+:-) Or, game version with falling divs, but you lose points if enemies hit bottom
+
+
+Other Todos:  
+
+:-) Make into my website with this just as a page
+
+
---
+++ server.js
@@ -0,0 +1,129 @@
+//assign and require express, body parser, cors and mongoose
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const shortId = require('shortid');
+
+
+//create mongoose schema for users and their data
+var schema = new Schema({
+    playerName: String,
+    score: Number,
+    shortId: {type: String, unique: true, default: shortId.generate}
+});
+
+//create mongoose model from schema
+var Player = mongoose.model('Player', schema);
+
+//connect to db
+var db = mongoose.connect(process.env.MLAB_URI, function(err) {
+  if(err) {console.log("err");}
+});
+
+//allows app to access public static files
+app.use(express.static('public'))
+//with a req to / responds with views/index.html with any dependencies to public static files from line23.
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html')
+});
+
+//allows access to public static files
+app.use('/public', express.static(process.cwd() + '/public'));
+
+
+//cross-origin browsing middleware
+app.use(cors())
+
+
+/*----bodyParser middleware----*/
+//body-parser extracts the entire body portion of an incoming request stream and exposes it on req.body as something easier to interface with
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
+/*----routes----*/
+
+/*----post requests----*/
+
+//route for posting new user to db
+app.post('/api/space-invader',function (req, res) {
+  var playerInst = new Player ({   //https://www.journaldev.com/6270/mongodb-sort
+      playerName: req.body.playerName,
+      score: req.body.score
+      });
+  
+  playerInst.save(function(err) {
+      if(err) {
+        res.send("err saving to db.")
+        console.log("err saving to db");
+      } else {
+        res.send("successfully saved.")
+        console.log("successfully saved.");
+      }
+  });
+});
+
+/*---- get requests----*/
+
+app.get('/api/space-invader', function(req, res){
+
+  Player.find({}).sort('-score').limit(5).exec(function(err, docs) {
+    var array = []
+    for (var i = 0; i < 5; i++) {
+      var element = "<p>" + docs[i].playerName + ": " + docs[i].score + "</p>";
+      array.push (element);
+    }
+    var stringtoSend = array.join("");
+    console.log(stringtoSend);
+    res.send(stringtoSend);
+  });
+
+})
+
+
+//route for interrogating db
+
+/*----end of routes----*/
+
+/*----functions----*/
+
+function isValidDate(d) {
+  return d instanceof Date && !isNaN(d);
+}
+
+
+// Not found middleware
+app.use((req, res, next) => {
+  return next({status: 404, message: '404 not found error'})
+})
+
+
+// Error Handling middleware
+app.use((err, req, res, next) => {
+  let errCode, errMessage
+
+  if (err.errors) {
+    // mongoose validation error
+    errCode = 400 // bad request
+    const keys = Object.keys(err.errors)
+    // report the first validation error
+    errMessage = err.errors[keys[0]].message
+  } else {
+    // generic or custom error
+    errCode = err.status || 500
+    errMessage = err.message || 'Internal Server Error'
+  }
+  res.status(errCode).type('txt')
+    .send(errMessage)
+})
+
+const listener = app.listen(process.env.PORT, () => {
+  console.log('Your app is listening on port ' + listener.address().port)
+})
+
+
+
+
